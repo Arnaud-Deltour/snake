b4ae0024ef4a669319b90f2aa63532c903f93937
Arnaud Deltour <arnaud.deltour@etu.minesparis.psl.eu>
2024-11-07 11:40:21 +0100

    Improved quitting mechanism
    also added minor improvements

--- snake/main.py
+++ snake/main.py
@@ -1,12 +1,14 @@
 import pygame
 import argparse
 
-def windowsize(WIDTH,HEIGHT):
+def windowsize():
     # using argparse to change the window size if wanted
-
+    DEFAULT_WIDTH = 400
+    DEFAULT_HEIGHT = 300
+    
     parser = argparse.ArgumentParser(description='Window size.')
-    parser.add_argument('-w','--width', type=int, default=WIDTH, help="Takes an integer value for the width of the game.")
-    parser.add_argument('-e','--height', type=int, default=HEIGHT, help="Takes an integer value for the height of the game.")
+    parser.add_argument('-w','--width', type=int, default=DEFAULT_WIDTH, help="Takes an integer value for the width of the game.")
+    parser.add_argument('-e','--height', type=int, default=DEFAULT_HEIGHT, help="Takes an integer value for the height of the game.")
     args = parser.parse_args()
 
     return args
@@ -14,29 +16,31 @@ def windowsize(WIDTH,HEIGHT):
 def snake():
     # main function for the game
 
-    WIDTH = 400
-    HEIGHT = 300
-
-    size = windowsize(WIDTH,HEIGHT)
+    # initialisation
+    size = windowsize()
     pygame.init()
     screen = pygame.display.set_mode( (size.width, size.height) )
     clock = pygame.time.Clock()
     pygame.display.set_caption("Snake")
 
-    while True:
+    game_running = True
+
+    # game loop
+    while game_running:
         clock.tick(10)
 
         for event in pygame.event.get():
 
             # quit game
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit(0)
-            if event.type == pygame.KEYDOWN:
+                game_running = False
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
-                    pygame.quit()
-                    quit(0)
+                    game_running = False
     
         screen.fill( (255, 255, 255) )
 
         pygame.display.update()
+
+    pygame.quit()
+    quit(0)
