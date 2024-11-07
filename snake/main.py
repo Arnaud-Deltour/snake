5c11ecb03b3c2d79e58b2d0fcec7b748a349f05b
Arnaud Deltour <arnaud.deltour@etu.minesparis.psl.eu>
2024-11-07 10:37:41 +0100

    Started the main structure and added size entry

--- snake/main.py
+++ snake/main.py
@@ -1,9 +1,33 @@
+import pygame
 import argparse
 
-def hello():
-    parser = argparse.ArgumentParser(description='Some description.')
-    parser.add_argument('-a', help="A text explaining what is the use of the -a option and what type of value it takes.")
+def windowsize():
+    # using argparse to change the window size if wanted
+
+    parser = argparse.ArgumentParser(description='Window size.')
+    parser.add_argument('-w','--width', type=int, default=400, help="Takes an integer value for the width of the game.")
+    parser.add_argument('-e','--height', type=int, default=300, help="Takes an integer value for the height of the game.")
     args = parser.parse_args()
-    print(args)
-    print("The value of -a is accessed with args.a: " + args.a)
-    print('Hello World')
+
+    return args
+
+def snake():
+    # main function for the game
+
+    size = windowsize()
+    pygame.init()
+    screen = pygame.display.set_mode( (size.width, size.height) )
+    clock = pygame.time.Clock()
+    pygame.display.set_caption("Snake")
+
+    while True:
+        clock.tick(10)
+
+        for event in pygame.event.get():
+            pass
+    
+        screen.fill( (255, 255, 255) )
+
+        pygame.display.update()
+
+    pygame.quit()
