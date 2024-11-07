780339940ea50fb4151aac34f3abca272a7479c2
Arnaud Deltour <arnaud.deltour@etu.minesparis.psl.eu>
2024-11-07 11:24:10 +0100

    Added quit game option

--- snake/main.py
+++ snake/main.py
@@ -1,12 +1,12 @@
 import pygame
 import argparse
 
-def windowsize():
+def windowsize(WIDTH,HEIGHT):
     # using argparse to change the window size if wanted
 
     parser = argparse.ArgumentParser(description='Window size.')
-    parser.add_argument('-w','--width', type=int, default=400, help="Takes an integer value for the width of the game.")
-    parser.add_argument('-e','--height', type=int, default=300, help="Takes an integer value for the height of the game.")
+    parser.add_argument('-w','--width', type=int, default=WIDTH, help="Takes an integer value for the width of the game.")
+    parser.add_argument('-e','--height', type=int, default=HEIGHT, help="Takes an integer value for the height of the game.")
     args = parser.parse_args()
 
     return args
@@ -14,7 +14,10 @@ def windowsize():
 def snake():
     # main function for the game
 
-    size = windowsize()
+    WIDTH = 400
+    HEIGHT = 300
+
+    size = windowsize(WIDTH,HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode( (size.width, size.height) )
     clock = pygame.time.Clock()
@@ -24,10 +27,16 @@ def snake():
         clock.tick(10)
 
         for event in pygame.event.get():
-            pass
+
+            # quit game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit(0)
     
         screen.fill( (255, 255, 255) )
 
         pygame.display.update()
-
-    pygame.quit()
